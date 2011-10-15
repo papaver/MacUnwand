8c493d5d9afe5a0c6abc5511c6ee5d4f53564943
Moiz Merchant <papaver@gmail.com>
2011-10-15 16:17:13 -0700

    Initial setup.

---
+++ MacUnwandAppDelegate.m
@@ -0,0 +1,34 @@
+//
+//  MacUnwandAppDelegate.m
+//  MacUnwand
+//
+//  Created by Moiz Merchant on 10/15/11.
+//  Copyright 2011 Bunnies on Acid. All rights reserved.
+//
+
+//-----------------------------------------------------------------------------
+// imports
+//-----------------------------------------------------------------------------
+
+#import "MacUnwandAppDelegate.h"
+
+//-----------------------------------------------------------------------------
+// interface implementation
+//-----------------------------------------------------------------------------
+
+@implementation MacUnwandAppDelegate
+
+//-----------------------------------------------------------------------------
+
+@synthesize window = mWindow;
+
+//-----------------------------------------------------------------------------
+
+- (void) applicationDidFinishLaunching:(NSNotification*)notification 
+{
+	// insert code here to initialize your application 
+}
+
+//-----------------------------------------------------------------------------
+
+@end
---
+++ main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MacUnwand
+//
+//  Created by Moiz Merchant on 10/15/11.
+//  Copyright 2011 Bunnies on Acid. All rights reserved.
+//
+
+//-----------------------------------------------------------------------------
+// imports
+//-----------------------------------------------------------------------------
+
+#import <Cocoa/Cocoa.h>
+
+//-----------------------------------------------------------------------------
+// interface definition
+//-----------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
